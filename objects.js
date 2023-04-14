2129c4e707d53788df113a75de658548788dde4d
Jeremy Bohannon <jeremybohannon@outlook.com>
2023-04-13 20:27:02 -0400

    added objects file

---
+++ objects.js
@@ -0,0 +1,11 @@
+const myPhone = {
+   color: 'black',
+   numberOfCameras: 4,
+   model: 'iPhone 13 Pro Max',
+   isItInGoodCondition: true,
+   appsInstalled: ['slack','twitter','instagram', 'twitch'],
+   proofOfObjectInObject: {}
+ }
+
+console.log('myPhone', myPhone)
+console.log('my apps', myPhone.appsInstalled)
